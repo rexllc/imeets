a9e43197babe36652c395e5c2656564e5c784c0c
rexllc <rexluciano36@gmail.com>
2022-02-25 13:24:34 +0800

    feat: upgrade method to use a better and new integration.

--- app/src/main/java/gq/fora/app/models/friends/add/AddFriends.java
+++ app/src/main/java/gq/fora/app/models/friends/add/AddFriends.java
@@ -1,16 +1,14 @@
 package gq.fora.app.models.friends.add;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class AddFriends {
 
     private static AddFriends instance = null;
-
-    private FirebaseDatabase firebase = FirebaseDatabase.getInstance();
-    private DatabaseReference friends = firebase.getReference("friends");
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     public static AddFriends getInstance() {
         if (instance == null) {
@@ -24,38 +22,70 @@ public class AddFriends {
         map.put("isFriends", false);
         map.put("friendsId", following_id);
         map.put("status", "pending");
-        map.put("userId", follower_id);
-		map.put("senderId", follower_id);
-		map.put("receiverId", following_id);
-        friends.child(follower_id).child(following_id).updateChildren(map);
+        map.put("userId", following_id);
+        map.put("senderId", follower_id);
+        map.put("receiverId", following_id);
+        database.collection("friends")
+                .document(follower_id)
+                .collection("list")
+                .document(following_id)
+                .set(map);
         Map<String, Object> map2 = new HashMap<>();
         map2.put("isFriends", false);
         map2.put("status", "pending");
         map2.put("friendsId", follower_id);
         map2.put("userId", following_id);
-		map2.put("senderId", following_id);
-		map2.put("receiverId", follower_id);
-        friends.child(following_id).child(follower_id).updateChildren(map2);
+        map2.put("senderId", following_id);
+        map2.put("receiverId", follower_id);
+        database.collection("friends")
+                .document(following_id)
+                .collection("list")
+                .document(follower_id)
+                .set(map2);
     }
 
     public void Unfriend(String follower_id, String following_id) {
-        friends.child(follower_id).child(following_id).removeValue();
-        friends.child(following_id).child(follower_id).removeValue();
+        database.collection("friends")
+                .document(follower_id)
+                .collection("list")
+                .document(following_id)
+                .delete();
+        database.collection("friends")
+                .document(following_id)
+                .collection("list")
+                .document(follower_id)
+                .delete();
     }
 
     public void confirmFriends(String follower_id, String following_id) {
         Map<String, Object> map = new HashMap<>();
         map.put("isFriends", true);
         map.put("status", "friends");
-        friends.child(follower_id).child(following_id).updateChildren(map);
+		database.collection("friends")
+                .document(follower_id)
+                .collection("list")
+                .document(following_id)
+                .update(map);
         Map<String, Object> map2 = new HashMap<>();
         map2.put("isFriends", true);
         map2.put("status", "friends");
-        friends.child(following_id).child(follower_id).updateChildren(map2);
+        database.collection("friends")
+                .document(following_id)
+                .collection("list")
+                .document(follower_id)
+                .update(map2);
     }
 
     public void removeRequest(String follower_id, String following_id) {
-        friends.child(follower_id).child(following_id).removeValue();
-        friends.child(following_id).child(follower_id).removeValue();
+        database.collection("friends")
+                .document(follower_id)
+                .collection("list")
+                .document(follower_id)
+                .delete();
+        database.collection("friends")
+                .document(following_id)
+                .collection("list")
+                .document(follower_id)
+                .delete();
     }
 }
